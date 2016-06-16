2c55c58cda78b5166653f06417e06d2d92f6150f
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-15 21:03:55 -0700

    Make current_user available throughout app

--- app/controllers/lunar_shell/application_controller.rb
+++ app/controllers/lunar_shell/application_controller.rb
@@ -1,5 +1,12 @@
 module LunarShell
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    private
+
+    def current_user
+      @current_user ||= LunarShell::User.find_by_id(session[:current_user_id])
+    end
+    helper_method :current_user
   end
 end
--- app/helpers/lunar_shell/user_helper.rb
+++ app/helpers/lunar_shell/user_helper.rb
@@ -1,9 +1,5 @@
 module LunarShell
   module UserHelper
-    def current_user
-      @current_user ||= LunarShell::User.find_by_id(session[:current_user_id])
-    end
-
     def username
       current_user.try(:username)
     end
