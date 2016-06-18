ac4142a5da2419b0195fb9e26a5cd7ce166c77ac
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-18 15:59:25 -0700

    Set and clear user

--- app/controllers/lunar_shell/application_controller.rb
+++ app/controllers/lunar_shell/application_controller.rb
@@ -4,9 +4,21 @@ module LunarShell
 
     private
 
+    def clear_current_user!
+      session[:current_user_id] = nil
+      @current_user = nil
+    end
+    helper_method :clear_current_user!
+
     def current_user
-      @current_user ||= LunarShell::User.find_by_id(session[:current_user_id])
+      @current_user ||= set_current_user
     end
     helper_method :current_user
+
+    def set_current_user(id = nil)
+      session[:current_user_id] = id if id.present?
+      @current_user = LunarShell::User.find_by_id(session[:current_user_id])
+    end
+    helper_method :set_current_user
   end
 end
