b5db01ca3148612f2f392d584d4e65e83ff6ca70
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-19 11:58:44 -0700

    Update passwd

--- app/satellites/lunar_shell/passwd_satellite.rb
+++ app/satellites/lunar_shell/passwd_satellite.rb
@@ -1,17 +1,17 @@
 module LunarShell
   class PasswdSatellite < LunarShell::Satellite
-    def run
+    def run!
       user ? request_current_password : no_user_error
     end
 
     private
 
     def no_user_error
-      { error_message: 'You must be logged in to change your password.' }
+      self.output = 'You must be logged in to change your password.'
     end
 
     def request_current_password
-      { status: :request_current_password }
+      self.view_template = 'request_current_password'
     end
   end
 end
