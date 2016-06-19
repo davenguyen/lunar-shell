baa2a2456e92c8e1681f5a7751b83b1db8cd454a
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-19 11:49:20 -0700

    Update su

--- app/satellites/lunar_shell/su_satellite.rb
+++ app/satellites/lunar_shell/su_satellite.rb
@@ -1,24 +1,25 @@
 module LunarShell
   class SuSatellite < LunarShell::Satellite
-    def run
-      return no_username unless args?
+    def run!
+      return no_username_error unless args?
 
       self.user = LunarShell::User.find_by_username(args.first)
-      user ? request_password : user_not_found
+      user ? request_password : user_not_found_error
     end
 
     private
 
-    def no_username
-      { error_message: 'Must provide a username.' }
+    def no_username_error
+      self.output = 'Must provide a username.'
     end
 
     def request_password
+      self.view_template = 'request_password'
       {status: :request_password, user_id: user.id }
     end
 
-    def user_not_found
-      { error_message: 'User not found.' }
+    def user_not_found_error
+      self.output = 'User not found.'
     end
   end
 end
