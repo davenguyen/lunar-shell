1fe1a6e1b3e612a5ad4062f54473a7a5fc006b36
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-19 17:41:09 -0700

    Add admin; fix log_in!

--- test/support/satellite_integration_test.rb
+++ test/support/satellite_integration_test.rb
@@ -1,7 +1,11 @@
 class SatelliteIntegrationTest < ActionDispatch::IntegrationTest
+  def admin
+    @admin ||= lunar_shell_users(:admin)
+  end
+
   def log_in!(u = user)
-    run_command "su #{user.username}"
-    xhr :patch, "/satellites/su/#{user.id}", user: { password: 'password' }
+    run_command "su #{u.username}"
+    xhr :patch, "/satellites/su/#{u.id}", user: { password: 'password' }
   end
 
   def run_command(input)
