81128e80717d7ba29d0bdc1462da2ff7c1052a51
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-19 17:42:07 -0700

    su test

---
+++ test/integration/su_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class SuTest < SatelliteIntegrationTest
+  test "can log in" do
+    assert_not response.body.include?(user.username)
+    log_in!
+    assert response.body.include?(user.username)
+  end
+
+  test "can substitue user" do
+    assert_not response.body.include?(user.username)
+    assert_not response.body.include?(admin.username)
+
+    log_in!
+    assert response.body.include?(user.username)
+    assert_not response.body.include?(admin.username)
+
+    log_in!(admin)
+    assert response.body.include?(admin.username)
+  end
+end
