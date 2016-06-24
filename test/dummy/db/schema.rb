f2d81df36e25ccfeeda1b668b59826412ca28b51
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-24 01:46:25 -0700

    Was unnecessary to bring into test/dummy

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616024022) do
+ActiveRecord::Schema.define(version: 20160616015436) do
 
   create_table "lunar_shell_users", force: :cascade do |t|
     t.string   "username",        limit: 32,                 null: false
