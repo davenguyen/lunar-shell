ca938e8d930aca47e6e0d4608d23cb6f76f5bd3f
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-25 03:53:04 -0700

    Precompile assets so apps don't have to pull it into the main app

--- lib/lunar_shell/engine.rb
+++ lib/lunar_shell/engine.rb
@@ -5,5 +5,9 @@ module LunarShell
     initializer 'lunar_shell.add_middleware' do |app|
       app.middleware.insert_after Rack::ETag, LunarShell::Middleware::Interpreter
     end
+
+    initializer "lunar_shell.assets.precompile" do |app|
+      app.config.assets.precompile += %w(lunar_shell.coffee lunar_shell.sass)
+    end
   end
 end
