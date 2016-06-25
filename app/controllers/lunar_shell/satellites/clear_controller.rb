239ab9510897b24b0ef04efc279b071a667c01c8
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-25 01:50:07 -0700

    Clear should be a satellite, too

---
+++ app/controllers/lunar_shell/satellites/clear_controller.rb
@@ -0,0 +1,8 @@
+module LunarShell
+  module Satellites
+    class ClearController < LunarShell::SatellitesController
+      def run
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,15 @@
 LunarShell::Engine.routes.draw do
   namespace :satellites do
+    resource :clear, only: []
     resource :exit, only: []
-
     resource :help, only: []
-
     resource :history, only: []
-
     resource :passwd, only: [:create] do
       post 'confirm_password'
       post 'new_password'
     end
-
     resource :quit, only: []
-
     resource :su, only: [:create]
-
   end
   post 'satellites/*error', to: 'satellites#error'
 
