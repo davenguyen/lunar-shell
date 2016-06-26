8ecd231a53ad144dfdf98ef1dd6b202c6a9f203c
Dave Nguyen <Dave.Nguyen@inthenight.net>
2016-06-25 17:14:54 -0700

    Removed with 239ab9510897b24b0ef04efc279b071a667c01c8

--- app/controllers/lunar_shell/satellites_controller.rb
+++ app/controllers/lunar_shell/satellites_controller.rb
@@ -10,7 +10,6 @@ module LunarShell
 
     def error
       render 'no_command' and return unless command
-      render 'clear' and return if command == 'clear'
       render 'command_not_found'
     end
 
